bfb1b44285dc5fcdbb28b12be23adf85b8ac6c47
Nick Peterson <nick.peterson@slalom.com>
2025-02-01 23:40:36 -0800

    Working memory for real this time. No async summary anymore.

--- src/sanctioned/consultagent/sys_msg.py
+++ src/sanctioned/consultagent/sys_msg.py
@@ -1,4 +1,4 @@
-#sys_msg.py
+# sys_msg.py
 
 
 def chatbot_sys_msg(summary: str, existing_memory_content: str) -> str:
@@ -8,59 +8,55 @@ def chatbot_sys_msg(summary: str, existing_memory_content: str) -> str:
     return CHATBOT_SYSTEM_MESSAGE
 
 
-def summary_sys_msg(summary: str, memory:str) -> str:      
+def summary_sys_msg(summary: str) -> str:      
     SUMMARY_SYSTEM_MESSAGE = f"""You a helpful assistant that supports users with various end systems.
-    CURENT INTERACTION INFORMATION:
+    CURENT INTERACTION SUMMARY:
     {summary}
 
-    {memory}
 
     INSTRUCTIONS:
 
-    1. Review the chat history carefully and prioritize key:value pairs.
-    2. Identify new information about the interaction, such as:
+    1. Review the chat history and CURENT INTERACTION SUMMARY carefully. Prioritize key:value pairs.
+    2. Identify new information about the CURENT INTERACTION SUMMARY, such as:
         - Systemic Ids of any and all record types
         - New records of any type being created
         - Any updates to existing records
         - Any deletions of records
     3. The relationship between records is CRITICAL and must be accurately established or maintained
-    4. Merge any new information with existing summary
-    5. Format the summary as a clear, bulleted list
-    6. If new infrmation conflicts with existing summary, use the most recent information
+    4. Merge any new information with existing CURENT INTERACTION SUMMARY
+    5. Format the CURENT INTERACTION SUMMARY as a clear, bulleted list
+    6. If new infrmation conflicts with existing CURENT INTERACTION SUMMARY, use the most recent information
 
     Remember: Only include factual information either stated by the user or returned from any end system the user is interacting with.
               Do not make assumptions or inferences.
 
-    Based on the chat history below, plese update the summary with the most recent information.
+    Based on the chat history, CURENT INTERACTION SUMMAR please update the CURENT INTERACTION SUMMARY with the most recent information.
     """
     return SUMMARY_SYSTEM_MESSAGE
 
 
-def memory_sys_msg(memory: str, summary: str) -> str:
+def memory_sys_msg(memory: str) -> str:
     MEMORY_SYSTEM_MESSAGE = f"""You a helpful assistant that supports users with various end systems.
-    CURENT INTERACTION SUMMARY:
-    {summary}
-
     MEMORY OF RECORDS:
     {memory}
 
     INSTRUCTIONS:
 
-    1. Review the CURRENT INTERACTION SUMMARY and MEMORY OF RECORDS carefully and prioritize key:value pairs.
-    2. Identify new information about the interaction betweewn the CURRENT INTERACTION SUMMARY and MEMORY OF RECORDS, such as:
+    1. Review the chat history, summary and MEMORY OF RECORDS carefully.
+    2. Identify new information about the interaction betweewn the summary and MEMORY OF RECORDS, such as:
         - Systemic Ids of any and all record types
         - New records of any type being created
         - Any existing records that new records are related to
         - Any updates to existing records
         - Any deletions of records
     3. The relationship between records is CRITICAL and must be accurately established or maintained
-    4. Merge any new information from the CURRENT INTERACTION SUMMARY into MEMORY OF RECORDS
+    4. Merge any new information from the summary into MEMORY OF RECORDS
     5. Format the memory as a clear, bulleted list
     6. If new infrmation conflicts with existing memory, use the most recent information
 
     Remember: Only include factual information either stated by the user or returned from any end system the user is interacting with.
               Do not make assumptions or inferences.
 
-    Based on the CURRENT INTERACTION SUMMARY and MEMORY OF RECORDS, plese update the memory with the most recent information.
+    Based on the chat history, summary and MEMORY OF RECORDS, plese update the memory with the most recent information.
     """
     return MEMORY_SYSTEM_MESSAGE
